3adfafd9f1ef76e0011d496eb77a8362bf9edd9d
ymakuta <ymakuta@evolution.com>
2022-08-15 20:34:40 +0200

    add test

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
-console.log("GGGG")
+const firstNumber: number = 0;
+const secondNumber: number = 1;
+const lengthFib: number = 6;
+
+function getFib(first: number, second: number, amount: number): number[] {
+    let fibonacci = [first, second];
+    for (let i = 2; i < amount; i++) {
+        fibonacci[i] = fibonacci[i-2] + fibonacci[i-1];
+    }
+    return fibonacci
+}
+
+console.log(getFib(firstNumber, secondNumber, lengthFib));
